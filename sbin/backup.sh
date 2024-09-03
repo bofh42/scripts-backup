54eccbe46331d14041ae52a628fb1885ced11103
Peter Tuschy <bofh42+git@mailbox.org>
2024-09-03 15:44:30 +0200

    now git friendly etc/ is ignored and setup from lib/

--- sbin/backup.sh
+++ sbin/backup.sh
@@ -75,6 +75,16 @@ else
   exit 1
 fi
 
+for i in backup.conf exclude.pattern include.list ; do
+  if [ -f "${FHS}/etc/${i}" ]; then
+    : # this is fine we have a config file
+  else
+    cp "${FHS}/lib/${i}" "${FHS}/etc/${i}" \
+      && echo "INFO: copied ${FHS}/lib/${i} to ${FHS}/etc/${i}" \
+      || exit $?
+  fi
+done
+
 ALL="$@"
 
 #
